4dd56e66676822e983ac950c2da9c0c11e994ee0
naldoluis <javagithub@outlook.com>
2022-10-02 15:02:32 -0300

    funcoes e recursos para projetos futuro

--- frontend/src/services/rootReducer.js
+++ frontend/src/services/rootReducer.js
@@ -1,9 +1,9 @@
-import { combineReducers } from 'redux'
+import { createSlice } from '@reduxjs/toolkit'
 import userReducer from './user/userReducer'
 import authReducer from './user/auth/authReducer'
 import bookReducer from './book/bookReducer'
 
-const rootReducer = combineReducers({
+const rootReducer = createSlice({
   user: userReducer,
   book: bookReducer,
   auth: authReducer
